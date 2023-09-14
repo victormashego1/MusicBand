8deb353c0237923e8c63437cba85474825447e37
victormashego1 <mashegove@email.com>
2023-09-14 20:51:46 +0200

    musicband admin.py documentation

--- mysite/musicband/admin.py
+++ mysite/musicband/admin.py
@@ -1,5 +1,20 @@
+"""Models are registered here in order to access them in Django Admin.
+
+Import:
+
+from django.contrib import admin\n
+from .models import Music, Choice
+
+Models:
+
+admin.site.register(Music)\n
+admin.site.register(Choice)
+
+
+"""
 from django.contrib import admin
 from .models import Music, Choice
+
 # Register your models here.
 admin.site.register(Music)
 admin.site.register(Choice)
